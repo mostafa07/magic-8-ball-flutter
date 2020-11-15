68ea11f25fc7c213837e169000fc37f0b53dc9b5
Mostafa Mahmoud Salem <msalem@ejada.com>
2020-11-15 17:45:12 +0200

    feat: implemented app functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.blue.shade800,
+          title: Text('Ask Me Anything')),
+      body: Container(
+        color: Colors.blue,
+        child: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+
+  int ballNum;
+
+  @override
+  void initState() {
+    super.initState();
+    ballNum = 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$ballNum.png'),
+        onPressed: () {
+          setState(() {
+            ballNum = Random().nextInt(5) + 1;
+          });
+        }
+      ),
+    );
+  }
+}
